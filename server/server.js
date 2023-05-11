abe42e2ea1c3d1f740ad988fe77268d7ce9fbc9c
mustafa hamandosh <hamandoshm@hotmail.com>
2023-05-11 14:16:57 -0300

    refactor: redirect to dynamic shop URL in authentication

--- server/server.js
+++ server/server.js
@@ -89,7 +89,7 @@ app.prepare().then(async () => {
 
     // This shop hasn't been seen yet, go through OAuth to create a session
     if (ACTIVE_SHOPIFY_SHOPS[shop] === undefined) {
-      ctx.redirect(`/auth?shop=veratigo.myshopify.com`);
+      ctx.redirect(`/auth?shop=${shop}`);
     } else {
       await handleRequest(ctx);
     }
